e3e4901ff10feeaa05f128c3897864a387bc08ba
Sivan Becker <sivanbecker@gmail.com>
2017-10-23 13:13:18 +0300

    add tmux stuff

---
+++ tmux_env/conf.py
@@ -0,0 +1,69 @@
+################# TEMPLATES ###############
+tdict = {}
+# tdict['infinilab'] = _windows(
+#     names=[ 'trm1', 'trm2', 'trm3', 'shell', 'runserver', 'infinilab', 'dmns', 'db'],
+#     pane_cmds=['', '', '', './manage.py shell','./manage.py runserver 5005','ssh infinilab', 'ssh infinilab-daemons','ssh infinilab-db'],
+#     work_dir='/Users/sbecker/envs/infinilab/infinilab',
+#     tool='infinilab')
+
+# tdict['bro'] = _windows (
+#     names=['bro', 'bro-db', 'local-bro'],
+#     pane_cmds=['ssh bro', 'ssh bro-db', ''],
+#     work_dir='/Users/sbecker/envs/bro/bro',
+#     tool='bro'
+#     )
+
+# tdict['dhcpawn'] = _windows(
+#     names=['trm1', 'trm2', 'trm3','testserver', 'pgcli', 'celery worker', 'rabbitmq-server'],
+#     pane_cmds=['', '', '', 'cob testserver','pgcli dhcpawn', 'celery -A cob.celery.app.worker worker --loglevel=DEBUG -E -B','rabbitmq-server',],
+#     work_dir='/Users/sbecker/envs/dhcpawn/dhcpawn/',
+#     tool='dhcpawn')
+
+
+# tdict['p2'] = _windows(names=['term3'], pane_cmds=['ls -l'], work_dir='/Users/sbecker/envs/p2/p2', tool='p2')
+
+# tdict['p3'] = _windows(
+#     names=['trm1', 'trm2', 'trm3'],
+#     pane_cmds=['','',''],
+#     work_dir='/Users/sbecker/envs/p3/p3',
+#     tool='p3')
+
+# tdict['infradev'] = _windows(names=['infradev_cli_1','infradev_cli_2','trm3'],
+#                              pane_cmds=['cd infradev-cli/infradev_cli', 'cd infradev-cli/infradev_cli', ''],
+#                              work_dir='/Users/sbecker/envs/itzik/infradev/',
+#                              tool='infradev')
+
+# tdict['flask'] = _windows(names=['trm1','trm2','trm3'],
+#                           pane_cmds=['','',''],
+#                           work_dir='/Users/sbecker/envs/flask/app1',
+#                           tool='flask')
+
+# tdict['meeseeks'] = _windows(names=['trm1','trm2','trm3'],
+#                           pane_cmds=['','',''],
+#                           work_dir='/Users/sbecker/envs/flask/meeseeks/meeseeks',
+#                           tool='meeseeks')
+
+tdict['flask'] = {
+    'names': ['trm1','trm2'],
+    'pane_cmds': ['',''],
+    'work_dir': '/tmp/flask',
+    }
+
+tdict['scotty'] = {
+    'names': ['trm1','trm2'],
+    'pane_cmds': ['',''],
+    'work_dir': '/home/archhome/envs/scotty/scotty/',
+    }
+
+tdict['infinilab'] = {
+    'names': ['trm1','trm2'],
+    'pane_cmds': ['',''],
+    'work_dir': '/home/archhome/envs/infinilab/infinilab/',
+    }
+
+tdict['infradev_cli'] = {
+    'names': ['trm1','trm2'],
+    'pane_cmds': ['',''],
+    'work_dir': '/home/archhome/envs/infradev_cli/',
+    }
+#################### END TEMPLATES ##############
---
+++ tmux_env/start.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+
+import click
+import logbook
+import yaml
+from conf import tdict
+
+_logger = logbook.Logger(__name__)
+
+
+################# GENERAL FUNCS #############
+
+def _get_env_command(dir):
+    if os.path.exists(os.path.realpath(dir)+'/../.env3'):
+        return 'cd {} && source ../.env3/bin/activate'.format(dir)
+    elif os.path.exists(os.path.realpath(dir)+'/../.env2'):
+        return 'cd {} && source ../.env2/bin/activate'.format(dir)
+    elif os.path.exists(os.path.realpath(dir)+'/.env3'):
+        return 'cd {} && source .env3/bin/activate'.format(dir)
+    elif os.path.exists(os.path.realpath(dir)+'/.env2'):
+        return 'cd {} && source .env2/bin/activate'.format(dir)
+
+
+def _get_pane_command(cmds):
+    return '&&'.join(list(filter(None, cmds)))
+
+def _windows(names=[], pane_cmds=[], work_dir=None, layout='even-horizontal', tool=None, tdict=None):
+
+    if tdict:
+        if not tdict.get(tool):
+            raise ValueError("No tool %s in configuration file %s" % (tool, os.path.dirname(__file__) + '/conf.py'))
+        names = tdict[tool]['names']
+        pane_cmds = tdict[tool]['pane_cmds']
+        work_dir = tdict[tool]['work_dir']
+
+    w = dict()
+    w['windows'] = []
+    w['tool'] = tool
+    if not len(names) == len(pane_cmds):
+        raise ValueError('Windows and commands are not the same length {}:{}'.format(names,pane_cmds))
+    for index,name in enumerate(names):
+        w['windows'].append({'window_name': name,
+                             'layout':layout,
+                             'panes':[_get_pane_command([_get_env_command(work_dir),pane_cmds[index]])]})
+
+    w['work_dir'] = os.path.dirname(work_dir)
+    w['env'] = w['work_dir']+'.env'
+
+    return w
+
+###################### END GENEREAL FUNCS #################
+
+@click.command()
+@click.argument('tool')
+def develop(tool):
+    tmuxp = os.path.dirname(os.path.realpath(__file__)) + '/.tmuxp'
+    try:
+        w = _windows(tool=tool, tdict=tdict)
+    except ValueError as e:
+        click.secho(e.__str__(), fg='red')
+        exit(1)
+
+    stpath = w['work_dir'] + '/{}_tmuxconf.yml'.format(tool)
+    tmux_config_filename = os.path.abspath(stpath)
+    with open(tmux_config_filename, 'w') as f:
+        yaml.dump(_get_tmux_config(w), f)
+    os.chdir(tdict[tool]['work_dir'])
+    os.execve('/home/archhome/Scripts/.env3/bin/tmuxp',
+              [tmuxp, 'load', tmux_config_filename],
+              {**os.environ})
+
+
+def _get_tmux_config(tool_windows):
+    return {
+        'session_name': tool_windows['tool'],
+        'windows': tool_windows['windows'],
+    }
+
+if __name__=='__main__':
+    develop()
